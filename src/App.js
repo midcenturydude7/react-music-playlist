2115ea2218cd04b74e8a0921d3abb7626df22568
midCenturyDude7 <mgriffes@gmail.com>
2022-02-18 16:24:45 -0800

    Updated files to fix SongPlayer bug

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from "react";
-import Header from "./components/Header";
-import AddSong from "./components/AddSong";
-import SongList from "./components/SongList";
-import SongPlayer from "./components/SongPlayer";
-import { Grid, useMediaQuery, Hidden } from "@material-ui/core";
-import songReducer from "./reducer";
+import React from 'react';
+import AddSong from './components/AddSong';
+import Header from './components/Header';
+import SongList from './components/SongList';
+import SongPlayer from './components/SongPlayer';
+import { Grid, useMediaQuery, Hidden } from '@material-ui/core';
+import songReducer from './reducer';
 
 export const SongContext = React.createContext({
   song: {
-    id: "bef8a1ca-6db1-4f44-b47c-8da14c12b508",
-    title: "A Way Home",
-    artist: "Memorex Memories",
-    thumbnail: "http://img.youtube.com/vi/KbC46oJmLh4/0.jpg",
-    url: "https://www.youtube.com/watch?v=KbC46oJmLh4",
-    duration: 239
+    id: '65924030-ce39-47cd-8ca9-e2ebe45b41a8',
+    title: 'Resonance',
+    artist: 'HOME',
+    thumbnail: 'http://img.youtube.com/vi/8GW6sLrK40k/0.jpg',
+    url: 'https://www.youtube.com/watch?v=8GW6sLrK40k',
+    duration: 213
   },
   isPlaying: false
 });
@@ -21,46 +21,42 @@ export const SongContext = React.createContext({
 function App() {
   const initialSongState = React.useContext(SongContext);
   const [state, dispatch] = React.useReducer(songReducer, initialSongState);
-  const greaterThanSm = useMediaQuery(theme => theme.breakpoints.up("sm"));
-  const greaterThanMd = useMediaQuery(theme => theme.breakpoints.up("md"));
+  const greaterThanSm = useMediaQuery(theme => theme.breakpoints.up('sm'));
+  const greaterThanMd = useMediaQuery(theme => theme.breakpoints.up('md'));
 
   return (
     <SongContext.Provider value={{ state, dispatch }}>
-      <Hidden only="xs">
+      <Hidden only='xs'>
         <Header />
       </Hidden>
       <Grid container spacing={3}>
-        <Grid
-          style={{
+        <Grid 
+          style={{ 
             paddingTop: greaterThanSm ? 80 : 10
-          }}
-          item
-          xs={12}
+          }} 
+          item 
+          xs={12} 
           md={7}
         >
           <AddSong />
           <SongList />
         </Grid>
-        <Grid
+        <Grid 
           style={
-            greaterThanMd
-              ? {
-                  position: "fixed",
-                  width: "100%",
-                  right: 0,
-                  top: 70
-                }
-              : {
-                  position: "fixed",
-                  width: "100%",
-                  left: 0,
-                  bottom: 0
-                }
-          }
-          item
-          xs={12}
-          md={5}
-        >
+            greaterThanMd ? {
+              position: 'fixed',
+              width: '100%',
+              right: 0,
+              top: 70
+        } : {
+          position: 'fixed',
+          width: '100%',
+          left: 0,
+          bottom: 0
+        }} 
+        item 
+        xs={12} 
+        md={5}>
           <SongPlayer />
         </Grid>
       </Grid>
--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -1,93 +1,90 @@
-import React from "react";
-import QueuedSongList from "./QueuedSongList";
-import {
-  Card,
-  CardContent,
-  Typography,
-  IconButton,
-  Slider,
-  CardMedia,
-  makeStyles
-} from "@material-ui/core";
-import { SkipPrevious, PlayArrow, SkipNext, Pause } from "@material-ui/icons";
-import { SongContext } from "../App";
-import { useQuery } from "@apollo/react-hooks";
-import { GET_QUEUED_SONGS } from "../graphql/queries";
+import React from 'react';
+import QueuedSongList from './QueuedSongList';
+import { Card, 
+    CardContent, 
+    CardMedia, 
+    IconButton, 
+    makeStyles, 
+    Slider, 
+    Typography 
+} from '@material-ui/core';
+import { PlayArrow, SkipNext, SkipPrevious, Pause } from '@material-ui/icons';
+import { SongContext } from '../App';
+import { GET_QUEUED_SONGS } from '../graphql/queries';
+import { useQuery } from '@apollo/react-hooks';
+import ReactPlayer from 'react-player';
 
 const useStyles = makeStyles(theme => ({
-  container: {
-    display: "flex",
-    justifyContent: "space-between"
-  },
-  details: {
-    display: "flex",
-    flexDirection: "column",
-    padding: "0px 15px"
-  },
-  content: {
-    flex: "1 0 auto"
-  },
-  thumbnail: {
-    width: 150
-  },
-  controls: {
-    display: "flex",
-    alignItems: "center",
-    paddingLeft: theme.spacing(1),
-    paddingRight: theme.spacing(1)
-  },
-  playIcon: {
-    height: 38,
-    width: 38
-  }
+    container: {
+        display: 'flex',
+        justifyContent: 'space-between',        
+    },
+    details: {
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '0px 15px'
+    },
+    content: {
+        flex: '1 0 auto'
+    },
+    thumbnail: {
+        width: 150
+    },
+    controls: {
+        display: 'flex',
+        alignItems: 'center',
+        paddingLeft: theme.spacing(1),
+        paddingRight: theme.spacing(1)
+    },
+    playIcon: {
+        height: 38,
+        width: 38
+    }
 }));
 
 function SongPlayer() {
-  const { data } = useQuery(GET_QUEUED_SONGS);
-  const { state, dispatch } = React.useContext(SongContext);
-  const classes = useStyles();
+    const { data } = useQuery(GET_QUEUED_SONGS)
+    const { state, dispatch } = React.useContext(SongContext);
+    const classes = useStyles();
 
-  function handleTogglePlay() {
-    dispatch(state.isPlaying ? { type: "PAUSE_SONG" } : { type: "PLAY_SONG" });
-  }
+    function handleTogglePlay() {
+        dispatch(state.isPlaying ? { type: "PAUSE_SONG" } : { type: "PLAY_SONG" });
+    }
 
-  return (
-    <>
-      <Card variant="outlined" className={classes.container}>
-        <div className={classes.details}>
-          <CardContent className={classes.content}>
-            <Typography variant="h5" component="h3">
-              {state.song.title}
-            </Typography>
-            <Typography variant="subtitle1" component="p" color="textSecondary">
-              {state.song.artist}
-            </Typography>
-          </CardContent>
-          <div className={classes.controls}>
-            <IconButton>
-              <SkipPrevious />
-            </IconButton>
-            <IconButton onClick={handleTogglePlay}>
-              {state.isPlaying ? (
-                <Pause className={classes.playIcon} />
-              ) : (
-                <PlayArrow className={classes.playIcon} />
-              )}
-            </IconButton>
-            <IconButton>
-              <SkipNext />
-            </IconButton>
-            <Typography variant="subtitle1" component="p" color="textSecondary">
-              00:01:30
-            </Typography>
-          </div>
-          <Slider type="range" min={0} max={1} step={0.01} />
-        </div>
-        <CardMedia className={classes.thumbnail} image={state.song.thumbnail} />
-      </Card>
-      <QueuedSongList queue={data.queue} />
-    </>
-  );
+    return (
+        <>
+            <Card variant='outlined' className={classes.container}>
+                <div className={classes.details}>
+                    <CardContent className={classes.content}>
+                        <Typography variant='h5' component='h3'>
+                            {state.song.title}
+                        </Typography>
+                        <Typography variant='subtitle1' component='p' color='textSecondary'>
+                            {state.song.artist}
+                        </Typography>
+                    </CardContent>
+                    <div className={classes.controls}>
+                        <IconButton>
+                            <SkipPrevious />
+                        </IconButton>
+                        <IconButton onClick={handleTogglePlay}>
+                            {state.isPlaying ? <Pause className={classes.playIcon} /> : <PlayArrow className={classes.playIcon} />}
+                        </IconButton>
+                        <IconButton>
+                            <SkipNext />
+                        </IconButton>
+                        <Typography variant='subtitle1' component='p' color='textSecondary'>
+                            00:01:30
+                        </Typography>
+                    </div>
+                    <Slider type='range' min={0} max={1} step={0.01} />
+                </div>
+                <ReactPlayer url={ state.song.url } playing={ state.isPlaying } hidden />
+                <CardMedia className={classes.thumbnail} image={state.song.thumbnail} />
+            </Card>
+            <QueuedSongList queue={data.queue} />
+        </>
+    );
 }
 
 export default SongPlayer;
